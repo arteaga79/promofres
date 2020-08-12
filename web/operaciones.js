9f43f593a1fabe11ae042733a46748ff8032fe5b
Luis Arteaga <Luis Arteaga@hp>
2020-08-12 12:35:51 -0500

    update

---
+++ web/operaciones.js
@@ -0,0 +1,139 @@
+  var suma;
+function acumular(){     
+              
+ var puntos= parseInt(document.getElementById("puntos").value);
+ var valor=parseInt(document.getElementById("valor").value);
+     suma =parseInt(puntos)+ parseInt(valor);
+    document.getElementById("tpuntos").innerHTML= suma;
+
+if(suma<1000)
+    alert('aun no puedes elegir premio');
+else if(suma>=100)
+    alert('Ya puedes empezar a canjear productos')
+          
+}
+   
+          function canje1() {
+               const cl=1600;
+               var puntos= parseInt(document.getElementById("puntos").value);
+ var valor=parseInt(document.getElementById("valor").value);
+  suma =parseInt(puntos)+ parseInt(valor);
+  
+  if(suma<cl)
+  
+              alert('Puntos insuficientes');
+  
+  else if(suma>=cl)
+               var canje=parseInt(suma)-cl;
+            document.getElementById("tpuntos").innerHTML=canje;
+          
+            alert('Felicidades, codigo es 789651');
+         
+            
+                  }
+                  
+                  
+      function canje2(){
+          const cl2=1000;
+               var puntos= parseInt(document.getElementById("puntos").value);
+ var valor=parseInt(document.getElementById("valor").value);
+  suma =parseInt(puntos)+ parseInt(valor);
+  
+  if(suma<cl2)
+      alert('Puntos insuficientes');
+  else if(suma>=cl2)
+               var canje2=parseInt(suma)-cl2;
+            document.getElementById("tpuntos").innerHTML=canje2;
+            alert('Felicidades, codigo es 7rt651')
+         
+          
+      }
+              
+           
+              
+          
+            function canje3(){
+                const cl3=1500;
+               var puntos= parseInt(document.getElementById("puntos").value);
+ var valor=parseInt(document.getElementById("valor").value);
+  suma =parseInt(puntos)+ parseInt(valor);
+  
+  if(suma<cl3)
+      alert('Puntos insuficientes');
+  else if(suma>=cl3)
+               var canje3=parseInt(suma)-cl3;
+            document.getElementById("tpuntos").innerHTML=canje3;
+            alert('Felicidades, codigo espt49651')
+                
+            }
+                 
+
+function canje4(){
+     const cl4=1200;
+               var puntos= parseInt(document.getElementById("puntos").value);
+ var valor=parseInt(document.getElementById("valor").value);
+  suma =parseInt(puntos)+ parseInt(valor);
+  
+  if(suma<cl4)
+      alert('Puntos insuficientes');
+  else if(suma>=cl4)
+               var canje4=parseInt(suma)-cl4;
+            document.getElementById("tpuntos").innerHTML=canje4;
+            alert('Felicidades, codigo kirt567')
+                
+}
+
+function canje5(){
+     const cl5=1000;
+               var puntos= parseInt(document.getElementById("puntos").value);
+ var valor=parseInt(document.getElementById("valor").value);
+  suma =parseInt(puntos)+ parseInt(valor);
+  
+  if(suma<cl5)
+      alert('Puntos insuficientes');
+  else if(suma>=cl5)
+               var canje5=parseInt(suma)-cl5;
+            document.getElementById("tpuntos").innerHTML=canje5;
+            alert('Felicidades, codigo jfur449651')
+                
+}
+ function canje6(){
+         const cl6=1000;
+               var puntos= parseInt(document.getElementById("puntos").value);
+ var valor=parseInt(document.getElementById("valor").value);
+  suma =parseInt(puntos)+ parseInt(valor);
+  
+  if(suma<cl6)
+      alert('Puntos insuficientes');
+  else if(suma>=cl6)
+               var canje6=parseInt(suma)-cl6;
+            document.getElementById("tpuntos").innerHTML=canje6;
+            alert('Felicidades, codigo rtega471')
+ }
+ 
+ function canje7(){
+         const cl7=1200;
+               var puntos= parseInt(document.getElementById("puntos").value);
+ var valor=parseInt(document.getElementById("valor").value);
+  suma =parseInt(puntos)+ parseInt(valor);
+  
+  if(suma<cl7)
+      alert('Puntos insuficientes');
+  else if(suma>=cl7)
+               var canje7=parseInt(suma)-cl7;
+            document.getElementById("tpuntos").innerHTML=canje7;
+            alert('Felicidades, codigo espt49651')
+ }  
+ function canje8(){
+         const cl8=1000;
+               var puntos= parseInt(document.getElementById("puntos").value);
+ var valor=parseInt(document.getElementById("valor").value);
+  suma =parseInt(puntos)+ parseInt(valor);
+  
+  if(suma<cl8)
+      alert('Puntos insuficientes');
+  else if(suma>=cl8)
+               var canje8=parseInt(suma)-cl8;
+            document.getElementById("tpuntos").innerHTML=canje8;
+            alert('Felicidades, codigo fdsf9d84f')
+ }
